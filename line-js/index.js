1c9296d2de52026955e13aa948d6ae62245ab093
Matt Trice <matt.trice@gmail.com>
2018-07-22 12:47:13 -0400

    Clean start

--- line-js/index.js
+++
@@ -1,5 +0,0 @@
-TweenMax.set("#abeja",{xPercent:-50, yPercent:-50})
-var motionPath = MorphSVGPlugin.pathDataToBezier('#pathAbeja', {align:"#abeja"}) ;
-
-
-TweenMax.to('#abeja', 10, {bezier:{values:motionPath, type:"cubic"}, ease:Linear.easeNone, repeat: -1});
